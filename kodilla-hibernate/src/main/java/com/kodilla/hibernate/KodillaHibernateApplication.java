10a04555e5ab04eb2bea352699a5a9c9dcc1e955
Katarzyna-Kantarowska <kantarowskakasia@gmail.com>
2024-12-11 14:17:31 +0100

    16.1 Hibernate

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -1,13 +1,13 @@
-package com.kodilla.patterns;
+package com.kodilla.hibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatternsApplication {
+public class KodillaHibernateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PatternsApplication.class, args);
+        SpringApplication.run(KodillaHibernateApplication.class, args);
     }
 
 }
